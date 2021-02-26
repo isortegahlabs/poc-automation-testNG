c876e0087473def0ddce9a37463c8044f9045094
isortegah <ivansebastian.ortega@bbva.com>
2021-02-25 21:50:42 -0600

    [ADD] First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+https://maven.apache.org/surefire/maven-surefire-plugin/examples/testng.html
+https://testng.org/doc/documentation-main.html
---
+++ src/main/java/me/isortegah/pocs/App.java
@@ -0,0 +1,15 @@
+package me.isortegah.pocs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "screen recording" );
+        System.out.println(new Calculator().add(5,5));
+    }
+}
