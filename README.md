b0a6836af234de84df357ba4e96915fabc065964
isortegah <ivansebastian.ortega@bbva.com>
2021-03-06 18:42:03 -0600

    [ADD] log4j2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 https://maven.apache.org/surefire/maven-surefire-plugin/examples/testng.html
 https://testng.org/doc/documentation-main.html
+
+Config log4j2 https://howtodoinjava.com/log4j2/log4j2-properties-example/
---
+++ src/test/me/isortegah/pocs/CaseOneTest.java
@@ -0,0 +1,24 @@
+package me.isortegah.pocs;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.testng.annotations.Test;
+
+import java.io.File;
+
+public class CaseOneTest {
+
+    private static final Logger logger = LogManager.getLogger(CaseOneTest.class);
+
+    @Test
+    public void stepOne(){
+        String message = "Hello there!";
+        logger.trace("Hello there trace!");
+        logger.debug("Hello there debug!");
+        logger.info("Hello there info!");
+        logger.warn("Hello there warn!");
+        logger.error("Hello there error!");
+        logger.fatal("Hello there fatal!");
+    }
+}
